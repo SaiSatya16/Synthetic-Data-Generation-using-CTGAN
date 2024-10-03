4a3cdf4f1053cf3ebd9be19ba0458e04514d2853
jj1039 <jj1039@srmist.edu.in>
2024-10-03 09:24:42 +0530

    initial commit

---
+++ api.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask import make_response, jsonify
+api=Api()
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+    
+
+
+
+
+
+api.add_resource(HelloWorld, '/')
